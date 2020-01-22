0e5d6ddab186b0550ddb7528e419b30825044bae
o.fareniuk <fareniuk.olexandr@gmail.com>
2020-01-22 21:44:24 +0200

    init(*): develop branch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './plugins/router'
+import vuetify from './plugins/vuetify';
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  
+  vuetify,
   render: h => h(App),
+  router,
 }).$mount('#app')
