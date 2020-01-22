e5be7a9a63068be17647b80813fdfc6fe67bc4f8
o.fareniuk <fareniuk.olexandr@gmail.com>
2020-01-22 22:47:12 +0200

    add(*): NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-
+  <v-app>
+    <NavBar />
+  </v-app>
 </template>
 
-
 <script>
+
+import NavBar from './components/NavBar/NavBar.vue'
+
 export default {
-  
+  components: {
+    NavBar
+  }
 }
+
 </script>
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-navigation-drawer app temporary>
+      <!-- -->
+        </v-navigation-drawer>
+
+        <v-app-bar 
+            app 
+            dense 
+            dark 
+            color="deep-purple accent-4"
+        > 
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Page title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+        </v-app-bar>
+
+        <v-content>
+
+            <router-view></router-view>
+        
+        </v-content>
+
+        <v-footer app>
+        <!-- -->
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
