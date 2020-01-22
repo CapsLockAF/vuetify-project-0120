d372054504cf9144755516f922ec0fd50f29506c
o.fareniuk <fareniuk.olexandr@gmail.com>
2020-01-23 01:10:10 +0200

    add(NavBar): menu icons

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,7 +1,27 @@
 <template>
     <div>
-        <v-navigation-drawer app temporary>
-      <!-- -->
+        <v-navigation-drawer 
+            app 
+            temporary
+            v-model="sideNav"
+        >
+            <v-list>
+                <v-list-item 
+                    v-for="(link, i) in links" 
+                    :key="i"
+                    :to="link.url"
+                >
+
+                <v-list-item-icon>
+                    <v-icon v-text="link.icon"></v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title v-text="link.title"></v-list-item-title>
+                </v-list-item-content>
+
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
 
         <v-app-bar 
@@ -10,14 +30,18 @@
             dark 
             color="deep-purple accent-4"
         > 
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon
+                @click="sideNav = !sideNav"
+            >
+            </v-app-bar-nav-icon>
 
             <v-toolbar-title>Page title</v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
+            <v-btn v-for="(link, i) in links" :key="i" color="transparent" :to="link.url" class="ma-1">
+                <v-icon left v-text="link.icon"></v-icon>
+                {{ link.title }}
             </v-btn>
 
         </v-app-bar>
@@ -36,6 +60,19 @@
 
 <script>
 export default {
+
+    data () {
+        return {
+            sideNav: false,
+            links: [
+                {title: 'Login', icon: 'mdi-account-box', url: '/login'},
+                {title: 'Registration', icon: 'mdi-face', url: '/register'},
+                {title: 'Cart', icon: 'mdi-cart', url: '/checkout'},
+                {title: 'New Product', icon: 'mdi-plus', url: '/new'},
+                {title: 'My Products', icon: 'mdi-book', url: '/list'}
+            ]
+        }
+    }
     
 }
 </script>
